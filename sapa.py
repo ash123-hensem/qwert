0b12990017508d711d45f69ce121f46fd8407da3
ash123-hensem <85070855+ash123-hensem@users.noreply.github.com>
2021-08-30 17:00:19 +0800

    Update sapa.py

--- sapa.py
+++ sapa.py
@@ -1,9 +1,13 @@
-from selenium import webdriver
-driver = webdriver.Chrome('C:\\webdrivers\\chromedriver.exe')
-driver.implicitly_wait(15) 
-driver.get('https://web.whatsapp.com')
-driver.find_element_by_css_selector("span[title='" + input("Enter name to spam: ") + "']").click()
-inputString = input("Enter message to send: ")
-while(True):
-    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]').send_keys(inputString)
-    driver.find_element_by_xpath('//*[@id="main"]/footer/div[1]/div[3]/button').click()
+  
+pyautogui==0.9.53
+import pyautogui
+import time
+
+count = 0
+pyautogui.click(10,5)
+while True:
+    pyautogui.FAILSAFE=True
+    pyautogui.write(f"Hey you there")
+    count +=1
+    pyautogui.press("ENTER")
+    time.sleep(1)
